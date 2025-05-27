efaf6fc3db46d13fe5d77ddc2910ac778f337de6
Iszreen17 <140799200+Iszreen17@users.noreply.github.com>
2025-05-27 13:56:19 +0800

    made some changes for scripts

---
+++ PlayerBehaviour.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+   private int score;
+private GameObject collectible;
+
+public void ModifyScore(int amount)
+{
+    score += amount;
+
+    if (score > 100) score = 100;
+    if (score < 0) score = 0;
+
+    Debug.Log($"Score is {score}");
+}
+
+void OnTriggerEnter(Collider other)
+{
+    if (other.CompareTag("Collectible"))
+    {
+        collectible = other.gameObject;
+    }
+}
+
+void OnTriggerExit(Collider other)
+{
+    if (other.gameObject == collectible)
+    {
+        collectible = null;
+    }
+}
+
+void OnInteract()
+{
+    var coinBehaviour = collectible.GetComponent<CoinBehaviour>();
+    coinBehaviour.Collect(this);
+}
+}
--- PlayerInputBehaviour.cs
+++ PlayerInputBehaviour.cs
@@ -1,38 +1,38 @@
-using Unity.Cinemachine;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class PlayerInputBehaviour : MonoBehaviour
-{
-    private int score;
-    
+public class PlayerBehaviour : MonoBehaviour
+{    private int score;
+    private GameObject collectible;
+
     public void ModifyScore(int amount)
     {
-        if (amount > 0 && score < 100)
-        {
-            score += amount;
-        }
-        else if(amount < 0 && score > 0)
-        {
-            score -= amount;
-        }
+        score += amount;
 
-        if(score > 100) score = 100;   
+        if (score > 100) score = 100;
         if (score < 0) score = 0;
 
+        Debug.Log($"Score is {score}");
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Collectable"))
+        if (other.CompareTag("Collectible"))
         {
-            collectable = other.gameObject;
+            collectible = other.gameObject;
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        
+        if (other.gameObject == collectible)
+        {
+            collectible = null;
+        }
+    }
+
+    void OnInteract()
+    {
+        var coinBehaviour = collectible.GetComponent<CoinBehaviour>();
+        coinBehaviour.Collect(this);
     }
 }
